95a2ea18c21f21c7c3c414c6bd2cce3e833abcbd
evShun <124552406+evShun@users.noreply.github.com>
2023-02-17 18:39:51 +0900

    完成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,6 @@ class PostsController < ApplicationController
 
   def create
     post = Post.create(content: params[:content])
-    render json{post: post}
-    redirect_to action: :index
+    render json:{post: post}
   end
 end
